71118d526311f3f5a478ce192dde3dfa8f8a2cd1
unknown <perezsebas@hotmail.com>
2018-07-06 08:29:03 -0500

    Form validation and styles

--- src/app/employees/new-employee/new-employee.component.ts
+++ src/app/employees/new-employee/new-employee.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
-import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft, faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 
 import { Employee } from '../../models/employee';
 import { AppState } from '../../models/app-state';
 
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
 
 import { NewEmployeeService } from './new-employee.service';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
@@ -22,40 +25,73 @@ import * as employeeActions from './../../actions/employee.actions';
 })
 export class NewEmployeeComponent implements OnInit {
   faChevronLeft = faChevronLeft;
+  faCalendar = faCalendarAlt;
 
   countries: any = [];
-
   employeesCollectionRef: AngularFirestoreCollection<Employee>;
-
   employeeForm: FormGroup;
+  employeeSelected: Employee;
+  employees;
+  formDisabled: boolean = false;
+
+  datePattern = "^\s*(3[01]|[12][0-9]|0?[1-9])\/(1[012]|0?[1-9])\/((?:19|20)\d{2})\s*$";
 
   constructor(
     private NewEmployeeService: NewEmployeeService,
     private store: Store<AppState>,
     public db: AngularFirestore,
-    private fb: FormBuilder) {
+    private route: ActivatedRoute,
+    private router: Router) {
 
     this.employeesCollectionRef = this.db.collection<Employee>('/employees');
-
-    this.createForm();
-  }
-
-  createForm() {
-    this.employeeForm = this.fb.group({
-      area: ['', Validators.required],
-      country: ['', Validators.required],
-      dab: ['', Validators.required],
-      hireDate: ['', Validators.required],
-      jobTitle: ['', Validators.required],
-      name: ['', Validators.required],
-      status: ['', Validators.required],
-      tipRate: ['', Validators.required],
-      username: ['', Validators.required]
-    });
   }
 
   ngOnInit() {
+    this.getParams();
     this.getCountries();
+  }
+
+  getParams() {
+    this.route.params.subscribe(params => {
+
+      if (params.action === 'view') {
+        this.formDisabled = true;
+      } 
+
+      this.createForm();
+
+      console.log(params);
+      if (params.actions === 'new') {
+        this.employeeForm.patchValue({ status: 'Active' });
+      } else {
+
+        this.employees = this.getValuesFromStore();
+        this.employees.states.employees.forEach(element => {
+          if (element.id === params.id) {
+            this.employeeSelected = element;
+          }
+        });
+        console.log(this.employeeSelected);
+        if (this.employeeSelected !== undefined) {
+          this.setFormValues(this.employeeSelected);
+        }
+      }
+    });
+  }
+
+  createForm() {
+    this.employeeForm = new FormGroup({
+      area: new FormControl({ value: '', disabled: this.formDisabled }, Validators.required),
+      country: new FormControl({ value: '0', disabled: this.formDisabled }, Validators.required),
+      dob: new FormControl({ value: '', disabled: this.formDisabled }, [Validators.required, Validators.pattern(this.datePattern)]),
+      hireDate: new FormControl({ value: '', disabled: this.formDisabled }, Validators.required),
+      jobTitle: new FormControl({ value: '0', disabled: this.formDisabled }, Validators.required),
+      name: new FormControl({ value: 'hola', disabled: this.formDisabled }, Validators.required),
+      status: new FormControl({ value: '', disabled: this.formDisabled }, Validators.required),
+      tipRate: new FormControl({ value: '', disabled: this.formDisabled }, Validators.required),
+      username: new FormControl({ value: '', disabled: this.formDisabled }, Validators.required)
+    });
+    console.log(this.employeeForm);
   }
 
   getCountries() {
@@ -65,9 +101,43 @@ export class NewEmployeeComponent implements OnInit {
       });
   }
 
-  addUser(newEmployee) {
-    this.employeesCollectionRef.add(newEmployee);   
-    this.store.dispatch(new employeeActions.AddEmployeeAction(newEmployee));
+  getValuesFromStore() {
+    let storeState;
+    this.store.subscribe(state => {
+      console.log(state);
+      storeState = state;
+    }).unsubscribe;
+    return storeState;
   }
 
+  setFormValues(employeeSelected: Employee) {
+    console.log(employeeSelected);
+    this.employeeForm.patchValue({
+      area: employeeSelected.area,
+      country: employeeSelected.country,
+      dob: employeeSelected.dob,
+      hireDate: employeeSelected.hireDate,
+      jobTitle: employeeSelected.jobTitle,
+      name: employeeSelected.name,
+      status: employeeSelected ? 'Active' : 'Inactive',
+      tipRate: employeeSelected.tipRate,
+      username: employeeSelected.username
+    });
+
+  }
+
+  addEmployee(newEmployee: Employee) {
+    this.employeesCollectionRef.add(newEmployee);
+    // this.store.dispatch(new employeeActions.AddEmployeeAction(newEmployee));
+    this.goToEmployeesComponent();
+  }
+
+  updateEmployee(employee: Employee) {
+    this.employeesCollectionRef.doc(employee.id).update({});
+    // this.store.dispatch(new employeeActions.EditEmployeeAction(employee));
+  }
+
+  goToEmployeesComponent() {
+    this.router.navigate(['/employees']);
+  }
 }
